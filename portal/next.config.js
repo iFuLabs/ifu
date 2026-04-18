1714efca55120d300872599ff8d0064729e1bd76
CalebAckom <calebackom@gmail.com>
2026-04-18 16:28:15 +0000

    Fix for Website and Portal modules

--- portal/next.config.js
+++ portal/next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: '/api/v1/:path*',
-        destination: `${process.env.API_URL || 'http://localhost:3000'}/api/v1/:path*`,
+        destination: `${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000'}/api/v1/:path*`,
       },
     ]
   },
